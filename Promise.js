243396ca88d744ee8f1eaebff74452b7a38c05b9
김동규 <54929514+Donggggg@users.noreply.github.com>
2021-09-28 13:10:43 +0900

    [feat] 생성자 및 resolve, reject 함수 구현

--- Promise.js
+++ Promise.js
@@ -1,5 +1,32 @@
+const status = {
+  PENDING: "pending",
+  FULFILLED: "fulfilled",
+  REJECTED: "rejected",
+  SETTLED: "settled",
+};
+
 export default class Promise {
-  constructor() {
+  state = status.PENDING;
+  result = undefined;
+
+  constructor(executor) {
     console.log("Promise 객체 생성");
+    executor(this.resolve.bind(this), this.reject.bind(this));
+  }
+
+  resolve(value) {
+    if (this.state !== status.PENDING) return;
+
+    this.result = value;
+    this.state = status.FULFILLED;
+    console.log("resolve");
+  }
+
+  reject(error) {
+    if (this.state !== status.PENDING) return;
+
+    this.result = error;
+    this.state = status.REJECTED;
+    console.log("reject");
   }
 }
--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 import Promise from "./Promise.js";
 
-const promise = new Promise();
+const promise = new Promise(function (resolve, reject) {
+  //   resolve(123);
+  setTimeout(() => resolve("done"), 1000);
+  setTimeout(() => reject(new Error("에러 발생!")), 1000);
+});
